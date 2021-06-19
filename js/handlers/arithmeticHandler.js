bb2e62a8cc0a12fde092aca1e0c807876a338596
Spencer Knight <spencer.knightusn@gmail.com>
2021-06-19 12:39:33 -0700

    compelted arithmetic handlers

---
+++ js/handlers/arithmeticHandler.js
@@ -0,0 +1,13 @@
+function arithmeticHandler(e) {
+  const CURRENT = document.querySelector("#current");
+  const TOTAL = document.querySelector("#total");
+  const REGEX = /^[\+\-\/x]$/g;
+
+  // if (REGEX.test(e.target.textContent) === true) {
+  if (REGEX.test(e.target.textContent) && CURRENT.textContent.length > 0) {
+    TOTAL.textContent += CURRENT.textContent + ` ${e.target.textContent} `;
+    CURRENT.textContent = "";
+  }
+}
+
+export { arithmeticHandler };
--- js/handlers/clearHandler.js
+++ js/handlers/clearHandler.js
@@ -1,6 +1,8 @@
 function clearHandler() {
   const CURRENT = document.querySelector("#current");
+  const TOTAL = document.querySelector("#total");
   CURRENT.textContent = "";
+  TOTAL.textContent = "";
 }
 
 export { clearHandler };
