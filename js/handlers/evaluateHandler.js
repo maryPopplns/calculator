49e1d3f9e359e4e12cb35bec5c84d7a96926668b
Spencer Knight <spencer.knightusn@gmail.com>
2021-06-20 12:57:18 -0700

    completed evaluateHandler

--- js/handlers/evaluateHandler.js
+++ js/handlers/evaluateHandler.js
@@ -1,5 +1,23 @@
 function evaluateHandler() {
-  console.log("evaluate");
+  const CURRENT = document.querySelector("#current");
+  const TOTAL = document.querySelector("#total");
+  const STRIPPED_TOTAL = TOTAL.textContent
+    .split("")
+    .splice(0, TOTAL.textContent.length - 3)
+    .join("")
+    .replace(/\s/g, "");
+  const STRIPPED_OVERALL = (TOTAL.textContent + CURRENT.textContent).replace(
+    /\s/g,
+    ""
+  );
+
+  if (CURRENT.textContent.length === 0) {
+    CURRENT.textContent = math.evaluate(STRIPPED_TOTAL);
+    TOTAL.textContent = "";
+  } else {
+    CURRENT.textContent = math.evaluate(STRIPPED_OVERALL);
+    TOTAL.textContent = "";
+  }
 }
 
 export { evaluateHandler };
