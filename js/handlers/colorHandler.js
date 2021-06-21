173d007c50e6ff8b85bc4e84e95dd0a9a4135481
Spencer Knight <spencer.knightusn@gmail.com>
2021-06-20 22:44:28 -0700

    finished colorChanger

--- js/handlers/colorHandler.js
+++ js/handlers/colorHandler.js
@@ -1,5 +1,20 @@
 function colorHandler(e) {
-  console.log(e);
+  const BODY = document.body;
+  if (e.target.id === "purple") {
+    BODY.style.backgroundColor = "#CC99C9";
+  }
+  if (e.target.id === "blue") {
+    BODY.style.backgroundColor = "#9EC1CF";
+  }
+  if (e.target.id === "green") {
+    BODY.style.backgroundColor = "#9EE09E";
+  }
+  if (e.target.id === "orange") {
+    BODY.style.backgroundColor = "#FEB144";
+  }
+  if (e.target.id === "red") {
+    BODY.style.backgroundColor = "#FF6663";
+  }
 }
 
 export { colorHandler };
