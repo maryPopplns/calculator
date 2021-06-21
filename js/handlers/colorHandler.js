f05b5e2a763f249ab08b7689edb8b8e066185b0d
Spencer Knight <spencer.knightusn@gmail.com>
2021-06-21 10:29:08 -0700

    completed mobile responsiveness

--- js/handlers/colorHandler.js
+++ js/handlers/colorHandler.js
@@ -2,19 +2,19 @@ function colorHandler(e) {
   const BODY = document.body;
   const ID = this.id;
   if (ID === "purple") {
-    BODY.style.backgroundColor = "rgb(204, 153, 201, 0.6)";
+    BODY.style.backgroundColor = "rgb(204, 153, 201, 0.8)";
   }
   if (ID === "blue") {
-    BODY.style.backgroundColor = "rgb(158, 193, 207, 0.6)";
+    BODY.style.backgroundColor = "rgb(158, 193, 207, 0.8)";
   }
   if (ID === "green") {
-    BODY.style.backgroundColor = "rgb(158, 224, 158, 0.6)";
+    BODY.style.backgroundColor = "rgb(158, 224, 158, 0.8)";
   }
   if (ID === "orange") {
-    BODY.style.backgroundColor = "rgb(254, 177, 68, 0.6)";
+    BODY.style.backgroundColor = "rgb(254, 177, 68, 0.8)";
   }
   if (ID === "red") {
-    BODY.style.backgroundColor = "rgb(255, 102, 99, 0.6)";
+    BODY.style.backgroundColor = "rgb(255, 102, 99, 0.8)";
   }
 }
 
