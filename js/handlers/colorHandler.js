aed5d72c89fed46c3fa7c9efcbbb219d57571a44
Spencer Knight <spencer.knightusn@gmail.com>
2021-06-21 09:21:12 -0700

    changed background color

--- js/handlers/colorHandler.js
+++ js/handlers/colorHandler.js
@@ -1,19 +1,20 @@
 function colorHandler(e) {
   const BODY = document.body;
-  if (e.target.id === "purple") {
-    BODY.style.backgroundColor = "#CC99C9";
+  const ID = this.id;
+  if (ID === "purple") {
+    BODY.style.backgroundColor = "rgb(204, 153, 201, 0.6)";
   }
-  if (e.target.id === "blue") {
-    BODY.style.backgroundColor = "#9EC1CF";
+  if (ID === "blue") {
+    BODY.style.backgroundColor = "rgb(158, 193, 207, 0.6)";
   }
-  if (e.target.id === "green") {
-    BODY.style.backgroundColor = "#9EE09E";
+  if (ID === "green") {
+    BODY.style.backgroundColor = "rgb(158, 224, 158, 0.6)";
   }
-  if (e.target.id === "orange") {
-    BODY.style.backgroundColor = "#FEB144";
+  if (ID === "orange") {
+    BODY.style.backgroundColor = "rgb(254, 177, 68, 0.6)";
   }
-  if (e.target.id === "red") {
-    BODY.style.backgroundColor = "#FF6663";
+  if (ID === "red") {
+    BODY.style.backgroundColor = "rgb(255, 102, 99, 0.6)";
   }
 }
 
